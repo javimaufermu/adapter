e6e44bcb596d9b8de980da03710a081867a8ae33
Javier_Mauricio_Fernandez <javimaufermu@unicauca.edu.co>
2021-09-04 23:15:08 -0500

    ejemplo completo
    	modified:   src/Demo.java
    	modified:   src/adapters/SquarePegAdapter.java
    	modified:   src/round/RoundHole.java
    	modified:   src/round/RoundPeg.java
    	modified:   src/square/SquarePeg.java

--- src/adapters/SquarePegAdapter.java
+++ src/adapters/SquarePegAdapter.java
@@ -1,5 +1,20 @@
 package adapters;
 
-public class SquarePegAdapter {
+import round.RoundPeg;
+import square.SquarePeg;
 
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
 }
